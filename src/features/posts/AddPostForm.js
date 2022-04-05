a57ffa1d0829bf016bb80eba2134e384a447412e
KyleKnolleLynch <kylelynch104@gmail.com>
2022-04-05 14:15:15 -0500

    implement createAsyncThunk and jsonplaceholder api

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,14 +1,16 @@
 import { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
-import { postAdded } from './postsSlice'
+import { addNewPost } from './postsSlice'
 import { selectAllUsers } from '../users/usersSlice'
 
 const AddPostForm = () => {
   const dispatch = useDispatch()
+
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [userId, setUserId] = useState('')
+  const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
   const users = useSelector(selectAllUsers)
 
@@ -16,17 +18,25 @@ const AddPostForm = () => {
   const onContentChanged = e => setContent(e.target.value)
   const onAuthorChanged = e => setUserId(e.target.value)
 
-  const onSavePostClicked = () => {
-    if (title && content) {
-      dispatch(postAdded(title, content, userId))
+  const canSave =
+    [title, content, userId].every(Boolean) && addRequestStatus === 'idle'
 
-      //   clear inputs and local state
-      setTitle('')
-      setContent('')
+  const onSavePostClicked = () => {
+    if (canSave) {
+      try {
+        setAddRequestStatus('pending')
+        dispatch(addNewPost({ title, body: content, userId })).unwrap()
+
+        setTitle('')
+        setContent('')
+        setUserId('')
+      } catch (error) {
+        console.error('Failed to save the post', error)
+      } finally {
+        setAddRequestStatus('idle')
+      }
     }
   }
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
   const usersOptions = users.map(user => (
     <option key={user.id} value={user.id}>
--- src/features/users/usersSlice.js
+++ src/features/users/usersSlice.js
@@ -1,15 +1,27 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-const initialState = [
-  { id: '0', name: 'Dude Lebowski' },
-  { id: '1', name: 'Mark Shelton' },
-  { id: '2', name: 'Dave Gray' },
-]
+const USERS_URL = 'https://jsonplaceholder.typicode.com/users'
+
+const initialState = []
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+  try {
+    return (await axios.get(USERS_URL)).data
+  } catch (error) {
+    return error.message
+  }
+})
 
 const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  extraReducers(builder) {
+    builder.addCase(fetchUsers.fulfilled, (state, action) => {
+      return action.payload
+    })
+  },
 })
 
 export const selectAllUsers = state => state.users
